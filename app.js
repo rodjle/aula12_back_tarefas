cb96398b25183f298bf2e64edf784b442d7f8291
unknown <rodjle@gmail.com>
2024-10-21 22:15:32 -0300

    ajuste yml2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
